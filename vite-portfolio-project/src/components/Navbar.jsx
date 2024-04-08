5c321a754ae184d329b373ba6ef15f382f8bba20
smbanati-IZxCKm <smbanati@gmail.com>
2024-04-08 17:49:50 -0400

    making navbar responsive

--- vite-portfolio-project/src/components/Navbar.jsx
+++ vite-portfolio-project/src/components/Navbar.jsx
@@ -1,16 +1,29 @@
+import { Component } from "react";
 import "../styles/Navbar.css";
 
-function Navbar() {
+class Navbar extends Component {
+  state={clicked: false};
+  handleClick = () =>{
+    this.setState({clicked: !this.state.clicked});
+  }
+  render() {
   return (
-    <nav className="Navbar">
-      <ul>
-        <li><a href="#" className="home-link">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-    </nav>
+    <div>
+      <nav className="Navbar">
+        <ul className={this.state.clicked? ".Navbar active" : '.Navbar'}>
+          <li><a href="#" className="home-link">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <div id="hamburger-mobile" onClick={this.handleClick}>
+       <i id="bar" className={this.state.clicked ? 
+       "fa-solid fa-x" : "fa-solid fa-bars"}></i>
+      </div>
+      </nav>
+    </div>
   );
+ }
 }
 
 export default Navbar;
